2eb469ad1acbe9f127c01e8457420f07ef70146d
“zhanjie” <zhanjie@dm-ai.com>
2021-09-24 18:53:22 +0800

    udpate code

--- code/source/utils/text-utils.h
+++ code/source/utils/text-utils.h
@@ -34,6 +34,23 @@
 
 namespace BASE_NAMESPACE{
 
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
+{
+    os << "[";
+    for (int i = 0; i < v.size(); ++i)
+    {
+        os << v[i];
+        if (i != v.size() - 1)
+        {
+            os << ", ";
+        }
+    }
+    os << "]";
+    return os;
+}
+
 template <class T>
 class NumberIstream{
  public:
