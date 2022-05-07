6bfb7f930a8538a0c90008fe8529349f9c8a5413
Zack <919353569@qq.com>
2022-05-07 18:05:07 +0800

    Update statistic.sh

--- shell/statistic.sh
+++ shell/statistic.sh
@@ -11,7 +11,7 @@ rm -rf statistic.txt
 while :
 do
     cpuRatio=`top -b -c -n 1 | grep $name | grep -v grep | head -n 1 | awk '{sub(/^\s+|\s+$/, "")}1' | awk '{print $9}'`
-    cpuMem=`ps -aux | grep $name | grep -v grep | awk '{print $6/1024}'`
+    cpuMem=`ps -aux | grep $name | grep -v grep | head -n 1 | awk '{print $6/1024}'`
     gpuRatio=`nvidia-smi | grep % | grep -v grep | head -n 1 | awk '{print $13}' | sed -e 's/%//g'`
     gpuMem=`nvidia-smi | grep $name | grep -v grep | head -n 1 | awk '{print $8}' | sed -e 's/MiB//g'`
     echo "$cpuRatio $cpuMem $gpuRatio $gpuMem" >> statistic.txt
